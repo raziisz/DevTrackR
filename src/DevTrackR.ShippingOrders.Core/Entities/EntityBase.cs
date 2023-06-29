987147161545eb055bc2c82f0b8780a98f160e64
Luiz Felipe <raziel_libertino@hotmail.com>
2023-06-29 16:11:19 -0400

    criado entidades, VOS e Enum

---
+++ src/DevTrackR.ShippingOrders.Core/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace DevTrackR.ShippingOrders.Core.Entities;
+
+public abstract class EntityBase
+{
+    public Guid Id { get; private set; }
+    public EntityBase()
+    {
+        Id = Guid.NewGuid();
+    }
+}
