4bfa63c156beae74e9a5deecf71b07a5232650d1
Luiz Felipe <raziel_libertino@hotmail.com>
2023-06-29 17:08:47 -0400

    config api

---
+++ src/DevTrackR.ShippingOrders.API/Controllers/ShippingServicesController.cs
@@ -0,0 +1,23 @@
+﻿using DevTrackR.ShippingOrders.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevTrackR.ShippingOrders.API;
+
+[ApiController]
+[Route("api/shipping-services")]
+public class ShippingServicesController : ControllerBase
+{
+    private readonly IShippingServiceService _service;
+
+    public ShippingServicesController(IShippingServiceService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll() {
+        var shippingServices = await _service.GetAll();
+
+        return Ok(shippingServices);
+    }
+}
