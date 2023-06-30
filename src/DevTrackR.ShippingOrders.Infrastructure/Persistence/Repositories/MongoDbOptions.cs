e82ca2145c8b3a02435671dfe2842afc70a93f78
Luiz Felipe <raziel_libertino@hotmail.com>
2023-06-30 13:00:16 -0400

    initial infra e pacotes

---
+++ src/DevTrackR.ShippingOrders.Infrastructure/Persistence/Repositories/MongoDbOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace DevTrackR.ShippingOrders.Infrastructure.Persistence.Repositories;
+
+public class MongoDbOptions
+{
+    public string ConnectionString { get; set; }    
+    public string Database { get; set; }
+}
