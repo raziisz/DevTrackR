e1e53de5165832a4fd3451d2fdb4a3a4e2005983
Luiz Felipe <raziel_libertino@hotmail.com>
2023-06-30 13:35:17 -0400

    implementacao repository

--- src/DevTrackR.ShippingOrders.Infrastructure/InfrastructureModule.cs
+++ src/DevTrackR.ShippingOrders.Infrastructure/InfrastructureModule.cs
@@ -10,48 +10,49 @@ public static class InfrastructureModule
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddMongo();
+        return services;
+    }
+    public static IServiceCollection AddMongo(this IServiceCollection services)
+    {
+        services.AddSingleton<MongoDbOptions>(sp =>
+        {
+            var configuration = sp.GetService<IConfiguration>();
+            var options = new MongoDbOptions();
 
+            configuration.GetSection("Mongo").Bind(options);
+
+            return options;
+        });
+
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var configuration = sp.GetService<IConfiguration>();
+            var options = sp.GetService<MongoDbOptions>();
+
+            var client = new MongoClient(options.ConnectionString);
+            var db = client.GetDatabase(options.Database);
+            
+            var dbSeed = new DbSeed(db);
+            dbSeed.Populate();
+
+            return client ;
+
+        });
+
+        services.AddTransient(sp =>
+        {
+            BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
+
+            var options = sp.GetService<MongoDbOptions>();
+            var mongoClient = sp.GetService<IMongoClient>();
+
+            var db = mongoClient.GetDatabase(options.Database);
+
+            return db;
+        });
+
+        return services;
     }
 }
 
-public static IServiceCollection AddMongo(this IServiceCollection services)
-{
-    services.AddSingleton<MongoDbOptions>(sp =>
-    {
-        var configuration = sp.GetService<IConfiguration>();
-        var options = new MongoDbOptions();
-
-        configuration.GetSection("Mongo").Bind(options);
-
-        return options;
-    });
-
-    services.AddSingleton<IMongoClient>(sp =>
-    {
-        var configuration = sp.GetService<IConfiguration>();
-        var options = sp.GetService<MongoDbOptions>();
-
-        var client = new MongoClient(options.ConnectionString);
-        var db = client.GetDatabase(options.Database);
-        
-        var dbSeed = new DbSeed(db);
-        dbSeed.Populate();
-
-        return client ;
-
-    });
-
-    services.AddTransient(sp =>
-    {
-        BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
-
-        var options = sp.GetService<MongoDbOptions>();
-        var mongoClient = sp.GetService<IMongoClient>();
-
-        var db = mongoClient.GetDatabase(options.Database);
-
-        return db;
-    });
-
-    return services;
-}
